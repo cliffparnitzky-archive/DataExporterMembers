fc53419614b17ce8b40efe5a69f78ed52c252b16
Cliff Parnitzky <github@cliff-parnitzky.de>
2012-12-05 10:11:15 +0100

    Initial

---
+++ README.md
@@ -0,0 +1,30 @@
+Contao DataExporterMembers Extension
+====================================
+
+Provides a special data exporter for members.
+
+
+Installation
+------------
+
+The extension is not published in contao extension repository.
+Install it manually.
+
+
+Tracker
+-------
+
+https://github.com/cliffparnitzky/DataExporterMembers/issues
+
+
+Compatibility
+-------------
+
+- min. version: Contao 2.9.5
+- max. version: Contao 2.11.6
+
+
+Dependency
+----------
+
+- This is a special data exporter. To use it the extension [[DataExporter]](https://github.com/cliffparnitzky/DataExporter) has to be installed.
---
+++ system/modules/DataExporterMembers/config/database.sql
@@ -0,0 +1,16 @@
+-- ********************************************************
+-- *                                                      *
+-- * IMPORTANT NOTE                                       *
+-- *                                                      *
+-- * Do not import this file manually but use the Contao  *
+-- * install tool to create and maintain database tables! *
+-- *                                                      *
+-- ********************************************************
+
+-- 
+-- Table `tl_content`
+-- 
+
+CREATE TABLE `tl_content` (
+  `dataExporterInvoiceLastDate` varchar(10) NOT NULL default ''
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
